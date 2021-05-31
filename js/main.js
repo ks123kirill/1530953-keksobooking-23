5f0ca9c1fa71427dbee02b5616c3a3cebde085d4
ks123kirill <ks123kirill@gmail.com>
2021-05-31 10:54:26 +0300

    Исправил функцию. Диапазон из близких дробных чисел

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 //https://learn.javascript.ru/task/random-int-min-max
 function randomInteger(min, max) {
-  if (min >= 0 && max >= 0 && min <= max) {
+  if (min >= 0 && max >= 0 && min < max) {
     const rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
+  return 'Ошибка! Введенные значения диапазона недопустимы!';
 }
 
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
 function randomFloat(min, max, countNumber) {
-  if (min >= 0 && max >= 0 && min <= max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    rand > max ? rand -= 1 : rand; // при умножении на Math.random(), rand может становится > max
-    rand < min ? rand = min : rand; // если min = 0 и мах = 0, то при выполнении тернарного оператора с условием rand > max, rand будет отрицательным
+  if (min >= 0 && max >= 0 && min < max && countNumber >= 0) {
+    const rand = min + Math.random() * (max - min);
     return rand.toFixed(countNumber);
   }
+  return 'Ошибка! Введенные значения диапазона недопустимы!';
 }
 
 randomInteger(1, 10);
-randomFloat(0, 5, 2);
+randomFloat(0.1, 0.11, 5);
