ee04a16c1d2c0188f1e9232b4812597cb459842a
ks123kirill <ks123kirill@gmail.com>
2021-06-23 16:35:27 +0300

    Использовал bool в условиях цикла

--- js/form-status.js
+++ js/form-status.js
@@ -4,26 +4,20 @@ const mapFilters = document.querySelector('.map__filters');
 const mapFiltersChildren = mapFilters.children;
 
 const statusActivityPage = function (isData) {
-  if (isData) {
-    adForm.classList.remove('ad-form--disabled');
-    for (let i = 0; i < adFormChildren.length; i++) {
-      adFormChildren[i].disabled = false;
-    }
-
-    mapFilters.classList.remove('ad-form--disabled');
-    for (let i = 0; i < mapFiltersChildren.length; i++) {
-      mapFiltersChildren[i].disabled = false;
-    }
-  } else {
+  if (!isData) {
     adForm.classList.add('ad-form--disabled');
-    for (let i = 0; i < adFormChildren.length; i++) {
-      adFormChildren[i].disabled = true;
-    }
-
     mapFilters.classList.add('ad-form--disabled');
-    for (let i = 0; i < mapFiltersChildren.length; i++) {
-      mapFiltersChildren[i].disabled = true;
-    }
+  } else {
+    adForm.classList.remove('ad-form--disabled');
+    mapFilters.classList.remove('ad-form--disabled');
+  }
+
+  for (let i = 0; i < adFormChildren.length; i++) {
+    adFormChildren[i].disabled = !isData;
+  }
+
+  for (let i = 0; i < mapFiltersChildren.length; i++) {
+    mapFiltersChildren[i].disabled = !isData;
   }
 };
 
