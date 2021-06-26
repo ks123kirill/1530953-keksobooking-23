dda7a9b718c610326851fa4ba5d01086019da674
ks123kirill <ks123kirill@gmail.com>
2021-06-26 23:05:58 +0300

    Добавил валидацию тип жилья и время заезда

--- js/form-validation.js
+++ js/form-validation.js
@@ -7,9 +7,19 @@ const adPriceInput = adForm.querySelector('#price');
 const adRoomNumberSelect = adForm.querySelector('#room_number');
 const adCapacitySelect = adForm.querySelector('#capacity');
 const adCapacityInputList = adCapacitySelect.children;
+const adTypeSelect = adForm.querySelector('#type');
+const adTimeInSelect = adForm.querySelector('#timein');
+const adTimeInList = adTimeInSelect.children;
+const adTimeOutSelect = adForm.querySelector('#timeout');
+const adTimeOutList = adTimeOutSelect.children;
 const keyValueList = {
   '100 rooms': '100',
   '0 guests': '0',
+  'bungalow': '0',
+  'flat': '5000',
+  'hotel': '3000',
+  'house': '5000',
+  'palace': '10000',
 };
 
 adTitleInput.addEventListener('input', () => {
@@ -29,6 +39,7 @@ adTitleInput.addEventListener('input', () => {
 
 adPriceInput.addEventListener('input', () => {
   const valuePriceInput = adPriceInput.value;
+
   if (valuePriceInput > MAX_PRICE_VALUE) {
     adPriceInput.setCustomValidity(`Максимальная стоимость ${MAX_PRICE_VALUE}`);
   }
@@ -49,7 +60,7 @@ const getDisabledCapacity = function () {
 /* getDisabledCapacity() устанавливает исходные настройки выбора: capacity options не имеющие атрибут selected, получают атрибут disabled. */
 getDisabledCapacity();
 
-const filterChangeHandler = function (evt) {
+const roomNumberChangeHandler = function (evt) {
   const valueRoomNumber = evt.target.value;
 
   for (let i = 0; i < adCapacityInputList.length; i++) {
@@ -68,4 +79,59 @@ const filterChangeHandler = function (evt) {
   }
 };
 
-adRoomNumberSelect.addEventListener('change', filterChangeHandler);
+adRoomNumberSelect.addEventListener('change', roomNumberChangeHandler);
+
+const typeChangeHandler = function (evt) {
+  adPriceInput.value = null;
+  const valueType = evt.target.value;
+
+  adPriceInput.placeholder = keyValueList[valueType];
+  adPriceInput.min = keyValueList[valueType];
+}
+
+adTypeSelect.addEventListener('change', typeChangeHandler);
+
+const timeInChangeHandler = function (evt) {
+  const valueTimeIn = evt.target.value;
+  for (let i = 0; i < adTimeOutList.length; i++) {
+    if (valueTimeIn === adTimeOutList[i].value) {
+      adTimeOutList[i].selected = true;
+    }
+    else {
+      adTimeOutList[i].selected = false;
+    }
+  }
+};
+
+adTimeInSelect.addEventListener('change', timeInChangeHandler);
+
+const timeOutChangeHandler = function (evt) {
+  const valueTimeOut = evt.target.value;
+  for (let i = 0; i < adTimeInList.length; i++) {
+    if (valueTimeOut === adTimeInList[i].value) {
+      adTimeInList[i].selected = true;
+    }
+    else {
+      adTimeInList[i].selected = false;
+    }
+  }
+};
+
+adTimeOutSelect.addEventListener('change', timeOutChangeHandler);
+
+//Как принять collection параметром вместе с evt?
+
+// const timeChangeHandler = function (evt, collection) {
+//   const valueTime = evt.target.value;
+//   for (let i = 0; i < collection.length; i++) {
+//     if (valueTime === collection[i].value) {
+//       collection[i].selected = true;
+//     }
+//     else {
+//       collection[i].selected = false;
+//     }
+//   }
+// };
+
+// adTimeInSelect.addEventListener('change', timeChangeHandler(adTimeOutList));
+// adTimeOutSelect.addEventListener('change', timeChangeHandler(adTimeInList));
