c6482e2c114480b5af480534ea1b081b57e3c21e
ks123kirill <ks123kirill@gmail.com>
2021-06-28 12:05:29 +0300

    Усовершенствовал объект keyTypeList

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,3 +1,5 @@
+import {keyTypeList} from './create-card.js';
+
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
 const MAX_PRICE_VALUE = 1000000;
@@ -9,20 +11,10 @@ const adCapacitySelect = adForm.querySelector('#capacity');
 const adCapacityInputList = adCapacitySelect.children;
 const adTypeSelect = adForm.querySelector('#type');
 const adTimeInSelect = adForm.querySelector('#timein');
-const adTimeInList = adTimeInSelect.children;
 const adTimeOutSelect = adForm.querySelector('#timeout');
-const adTimeOutList = adTimeOutSelect.children;
 const keyRoomGuestList = {
   '100 rooms': '100',
   '0 guests': '0',
-};
-
-const keyTypeList = {
-  'bungalow': '0',
-  'flat': '5000',
-  'hotel': '3000',
-  'house': '5000',
-  'palace': '10000',
 };
 
 adTitleInput.addEventListener('input', () => {
@@ -88,48 +80,20 @@ const typeChangeHandler = function (evt) {
   adPriceInput.value = null;
   const valueType = evt.target.value;
 
-  adPriceInput.placeholder = keyTypeList[valueType];
-  adPriceInput.min = keyTypeList[valueType];
+  adPriceInput.placeholder = keyTypeList[valueType].price;
+  adPriceInput.min = keyTypeList[valueType].price;
 };
 
 adTypeSelect.addEventListener('change', typeChangeHandler);
 
-// const timeInChangeHandler = function (evt) {
-//   const valueTimeIn = evt.target.value;
-//   for (let i = 0; i < adTimeOutList.length; i++) {
-//     adTimeOutList[i].selected = valueTimeIn === adTimeOutList[i].value;
-//   }
-// };
-
-// adTimeInSelect.addEventListener('change', timeInChangeHandler);
-
-// const timeOutChangeHandler = function (evt) {
-//   const valueTimeOut = evt.target.value;
-//   for (let i = 0; i < adTimeInList.length; i++) {
-//     adTimeInList[i].selected = valueTimeOut === adTimeInList[i].value;
-//   }
-// };
-
-// adTimeOutSelect.addEventListener('change', timeOutChangeHandler);
-
-const getSelectedTime = function (valueTime, collection) {
-  for (let i = 0; i < collection.length; i++) {
-    collection[i].selected = valueTime === collection[i].value;
-  }
+const timeInChangeHandler = function (evt) {
+  adTimeOutSelect.value = evt.target.value;
 };
 
-const timeChangeHandler = function (evt) {
-  const valueTime = evt.target.value;
-  let collection;
-  if (evt.target.id === 'timein') {
-    collection = adTimeOutList;
-  }
-  else {
-    collection = adTimeInList;
-  }
+adTimeInSelect.addEventListener('change', timeInChangeHandler);
 
-  getSelectedTime(valueTime, collection);
+const timeOutChangeHandler = function (evt) {
+  adTimeInSelect.value = evt.target.value;
 };
 
-adTimeInSelect.addEventListener('change', timeChangeHandler);
-adTimeOutSelect.addEventListener('change', timeChangeHandler);
+adTimeOutSelect.addEventListener('change', timeOutChangeHandler);
